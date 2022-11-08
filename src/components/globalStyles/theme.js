e6de3dd46602848d1427ec3662902a589de987c3
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-08 14:51:00 +0300

    Navbar finished

--- src/components/globalStyles/theme.js
+++ src/components/globalStyles/theme.js
@@ -1,6 +1,12 @@
-const teme = {
+const theme = {
     colors:{
         navbarBgColor:"#e1f1dd",
-        mainColor:'#00adb5'
+        mainColor:'#00adb5',
+        logoColor:'#393e46'
+    },
+    screens:{
+        lg:"835px"
     }
+
 }
+export default theme;
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Nav } from './Navbar.style'
+import React,{useState} from "react";
+import "./Navbar.style";
+import Nav, { Brand ,Menu, MenuLink, Hamburger } from "./Navbar.style";
+import { GiHamburgerMenu } from "react-icons/gi"
 
 const Navbar = () => {
+  const [isOpen,setIsOpen]=useState(false)
   return (
     <Nav justify="space-between" wrap="wrap">
-      <a href=""><i>{"<Clarusway/>"}</i><span>Recipe</span></a>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/logout">Logout</Link>        
-      </div>
-    </Nav>
-  )
-}
+      <Brand to="/">
+        <i>{"<Clarusway/>"}</i>
+        <span>Recipe</span>
+      </Brand>
 
-export default Navbar
+      <Hamburger onClick={()=>setIsOpen(!isOpen)}>
+        <GiHamburgerMenu/>
+      </Hamburger>
+
+      <Menu isOpen={isOpen} onClick={()=>setIsOpen(false)}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="Logout" onClick={()=>sessionStorage.clear()}>Logout</MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
+
+export default Navbar;
