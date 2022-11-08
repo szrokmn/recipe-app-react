43e962f5c8ea6403bea9e1ac490545ad34c1b3cd
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-08 10:09:58 +0300

    Global Styles finished

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
+import Navbar from "../components/nav/Navbar";
+import Home from "../pages/home/Home"
+import Login from "../pages/login/Login"
+import About from "../pages/about/About"
+import Detail from "../pages/detail/Detail"
 
 const AppRouter = () => {
   return (
-    <div>
-      App Router
-    </div>
-  )
-}
+    <BrowserRouter>
+    <GlobalStyles/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
