fb7b8d3dc72ae50cbd9a940bdf1c536207cf3aae
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-08 12:59:38 +0300

    login finished

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {FormContainer, Header, LoginContainer, StyledButton, StyledForm, StyledImg, StyledInput} from "./Login.style"
+import meal from "../../assets/meal.svg"
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+const navigate = useNavigate();
+
+const userInfo = {
+  username:"admin"
+}
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo))
+    navigate(-1)
+  }
   return (
-    <div>Login</div>
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal}/>
+        <Header>{"<CW/>"}Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder='Enter Username' type="text"/>
+          <StyledInput placeholder='Enter Password' type="password"/>
+          <StyledButton type='submit'>Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
   )
 }
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,24 +2,26 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
 import Navbar from "../components/nav/Navbar";
-import Home from "../pages/home/Home"
-import Login from "../pages/login/Login"
-import About from "../pages/about/About"
-import Detail from "../pages/detail/Detail"
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import About from "../pages/about/About";
+import Detail from "../pages/detail/Detail";
 import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-    <GlobalStyles/>
+      <GlobalStyles />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<PrivateRouter />}>
-          <Route path="" element={<About/>}/>
+          <Route path="" element={<About />} />
         </Route>
-        <Route path="/detail" element={<Detail />} />
+        <Route path="/about" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
