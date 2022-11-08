4743f911a1c4530662656fd5f40bb505d4634962
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-08 20:54:24 +0300

    Detail.jsx and style finished

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Button, Card, Header, Image, MainContainer } from "./Cards.style";
 import defaultImage from "../../assets/default-image.jpg"
 import {useNavigate} from "react-router-dom"
+
 const Cards = ({ recipes }) => {
+
   const navigate = useNavigate();
+  
   return (
     <MainContainer wrap="wrap">
       {recipes.map(({ recipe }, index) => {
